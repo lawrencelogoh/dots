c59003a2002d9ddb75ae97ecfa5c5d4d79ff4f00
Larry Logoh <lawrence.logoh@gmail.com>
2020-07-31 00:47:06 +0000

    minor changes to emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,4 +45,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "ADBO" :family "Fira Code Retina")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "ADBO" :family "Fira Code Retina"))))
+ '(italic ((t (:slant italic :family "Deja Vu Sans")))))

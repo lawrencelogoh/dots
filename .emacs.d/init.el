940ea5ba0415600fa16c7157e263f955366af9e3
Lawrence Logoh <lawrence.logoh@gmail.com>
2021-05-31 21:32:07 +0000

    Modified emacsd and pkglist

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(modus-themes emmet-mode eglot go-mode org-static-blog htmlize fira-code-mode format-all pomodoro company golint magit use-package)))
+   '(rust-mode haskell-mode python-mode reformatter modus-themes emmet-mode eglot go-mode org-static-blog htmlize fira-code-mode pomodoro company golint magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
